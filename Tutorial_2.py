5e2a84cdc12d71e837ab21bdf29e01241fb83431
Arjun Kashyap <arj.kashyap123@gmail.com>
2019-12-10 11:08:22 +0530

    tutorial 3

--- Tutorial_2.py
+++ Tutorial_2.py
@@ -1,17 +1,19 @@
+#!/usr/bin/python3.5
 # Regular expressions tutorial number 2.
 
 # Matching regex using the pipe method
 # a | b is used to specify a or b 
 import re
 
-regEx = re.compile(r'Batman|Superman')
+regEx = re.compile(r'[b/B]atman|[s/S]uperman')
 s = str(input("Enter Batman or Superman: "))
 mo = regEx.search(s)
 
 print("Expression matched: " + str(mo.group()))
 
 s = str(input("Enter anything related to bat ex: batmobile "))
-re2 = re.compile(r'[B|b]at[mobile|cave|man|woman]')
+re2 = re.compile(r'([B|b])at(mobile|cave|man|woman)')
 mo2 = re2.search(s)
 
 print("Expression matched " + str(mo2.group()))
+print("Exp group index 1: " + str(mo2.group(1)))
---
+++ Tutorial_3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.5
+#Optional matching with question mark ?
+
+import re
+
+# ? is preceded by an expression which is optional
+regEx = re.compile(r'[b/B]at(wo)?man')
+
+s = str(input("Batman/woman: "))
+mo = regEx.search(s)
+print("Phone number: " + str(mo.group()))
+
+regEx2 = re.compile(r'(\+\d{2})?( )?(\d{10})')
+s2 = str(input("Enter phone number: "))
+mo2 = regEx2.search(s2)
+print("Phone number: " + mo2.group())
